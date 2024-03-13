dc192a59a7a06f0c5029ae337512cd6f66da9553
Karen Ferreira <karenvsferreira@outlook.com>
2024-03-13 18:21:46 -0300

    first commit

---
+++ numbers_operators/script.js
@@ -0,0 +1,32 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+var total = 35;
+// Crie duas expressões que retornem NaN
+// Somar a string '200' com o número 50 e retornar 250
+// Incremente o número 5 e retorne o seu valor incrementado
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2; // NaN (Not a Number)
+console.log(pesoPorDois);
+
+var soma1 = "Amor" / "Ódio";
+console.log(soma1);
+ var soma2 = numero / unidade;
+ console.log(soma2);
+
+var duzentos = "200";
+var cinquenta = 50;
+var soma3 = (+duzentos + cinquenta);
+console.log(soma3);
+
+var numero5 = 5;
+console.log(++numero5);
+
+var dividir = (+numero / 2) + unidade;
+console.log(dividir);
+
+
+
+
---
+++ variables/script.js
@@ -0,0 +1,12 @@
+var nome = "Karen";
+var idade = 24;
+var comida;
+comida = "Pizza";
+var anoNascimento;
+var estadoCivil;
+var CEP;
+var horasTrabalhadas = 100;
+var valorHora = 100;
+let salario = (horasTrabalhadas * valorHora);
+
+
