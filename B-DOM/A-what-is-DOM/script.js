e0ac0d341d162f736d6d3c957bfd8ee430b133e8
Karen Ferreira <karenvsferreira@outlook.com>
2024-09-13 14:32:43 -0300

    organize

---
+++ B-DOM/A-what-is-DOM/script.js
@@ -0,0 +1,12 @@
+// Retorne o url da página atual utilizando o objeto window
+const hrefPagina = window.location.href;
+// console.log(hrefPagina);
+// Seleciona o primeiro elemento da página que possua a classe ativo
+const elementoAtivo = document.querySelector(".ativo");
+// console.log(elementoAtivo);
+// Retorne a linguagem do navegador
+const linguagemNavegador = window.navigator.language;
+// console.log(linguagemNavegador);
+// Retorne a largura da janela
+const larguraJanela = window.innerWidth;
+// console.log(larguraJanela);
